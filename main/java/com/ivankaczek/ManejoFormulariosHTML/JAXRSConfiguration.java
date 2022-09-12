58a6969b53abb2e1d2d77b7357d747e2de201a09
Ivan Kaczek <ivan.kaczek@gmail.com>
2022-09-12 18:48:59 -0300

    formulario simple en html

---
+++ main/java/com/ivankaczek/ManejoFormulariosHTML/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.ivankaczek.ManejoFormulariosHTML;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
